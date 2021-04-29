2fe2ee51c71284d6f206a2b198fcf24bf430fce0
Andreas Amstutz <tullo@users.noreply.github.com>
2021-04-29 13:16:01 +0200

    add policy on the many-to-many relationship

--- db/migrations/000009_create_order_products_table.up.sql
+++ db/migrations/000009_create_order_products_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE order_products
 (
-   order_id integer REFERENCES orders,
-   product_id integer REFERENCES products,
+   order_id integer REFERENCES orders ON DELETE CASCADE,
+   product_id integer REFERENCES products ON DELETE RESTRICT,
    PRIMARY KEY (product_id, order_id)
 );
