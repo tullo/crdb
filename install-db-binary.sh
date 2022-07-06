7becc36c811a622b9cb5d8f5608d6e874d266b34
Andreas Amstutz <tullo@users.noreply.github.com>
2022-07-06 08:55:41 +0200

    install cockroach linux binary

---
+++ install-db-binary.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BINARY=cockroach-v22.1.2.linux-amd64
+TEMPDIR=/tmp/crdb
+
+echo "Installing $BINARY"
+mkdir -p $TEMPDIR /usr/local/lib/cockroach &&
+wget -O $TEMPDIR/crdb.tgz https://binaries.cockroachdb.com/$BINARY.tgz &&
+tar -zxf $TEMPDIR/crdb.tgz -C $TEMPDIR &&
+install $TEMPDIR/$BINARY/cockroach /usr/local/bin/ &&
+install $TEMPDIR/$BINARY/lib/libgeos.so /usr/local/lib/cockroach &&
+install $TEMPDIR/$BINARY/lib/libgeos_c.so /usr/local/lib/cockroach &&
+rm -rf $TEMPDIR
+
+cockroach version
