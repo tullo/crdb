1813a2bce252615ae3d08255861b91f2c1c09c9f
Andreas Amstutz <tullo@users.noreply.github.com>
2023-07-20 22:46:28 +0200

    add ci test

---
+++ ci_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+func TestWithGithubService(t *testing.T) {
+	var (
+		db  *sql.DB
+		dsn = fmt.Sprintf("postgresql://postgres_user:postgres_password@postgres:%d/postgres_db?sslmode=disable", 5432)
+	)
+	// setup database connection
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		panic(err)
+	} else if err := db.Ping(); err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 5; i++ {
+		// query database
+		firstQueryStart := time.Now()
+		rows, err := db.Query("select 1;")
+		firstQueryEnd := time.Now()
+		if err != nil {
+			panic(err)
+		}
+
+		// put the connection back to the pool so
+		// that it can be reused by next iteration
+		rows.Close()
+
+		fmt.Println(fmt.Sprintf("query #%d took %s", i, firstQueryEnd.Sub(firstQueryStart).String()))
+	}
+}
